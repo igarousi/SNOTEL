51425f7645717c6caec4ee29a3cdf59478cc730c
igarousi <i.garousi@aggiemail.usu.edu>
2020-05-28 08:01:46 -0700

    Batch script to do setups when running jupyter lab on COMET. This is needed becasue the python version associated with singularity image is greater than the versions that have ulmo library. Ulmo library is needed to retrieve data from CUAHSI data client service.

--- code/conda_settings.sh
+++ code/conda_settings.sh
@@ -3,15 +3,17 @@
 which python
 python --version
 
-conda create --name snotel python=3.6 pip 
+conda create --name snotel python=3.6 pip --y 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate snotel
 
+conda install ipykernel --quiet --yes
+ipython kernel install --user --name=snotel
+
+pip install ulmo --quiet 
+conda install -c conda-forge timezonefinder --yes
+conda install matplotlib --yes
+conda install geopandas --yes
+
 which python
 python --version
-
-pip install ulmo
-pip install tzwhere
-conda install matplotlib
-conda install geopandas
-
